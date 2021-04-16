9f0d7b5d48756ef6d163269c7ecae77288601c6f
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-04-16 12:52:56 +0800

    Simple scrollview control with avplayerview controller

--- TikTokCloneUIKit/HomeViewController.swift
+++ TikTokCloneUIKit/HomeViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+
 
 class HomeViewController: UIViewController {
     var scrollView: UIScrollView!
@@ -14,9 +16,37 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
         scrollView = UIScrollView(frame: view.bounds)
         scrollView.backgroundColor = UIColor.black
-        scrollView.contentSize = view.bounds.size
         scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(scrollView)
+        
+        var yPos = 0
+        VideoSource.sharedInstance.videos.forEach { (video) in
+            let playerViewController =  makePlayerViewController(player: video.player, yPos:CGFloat(yPos))
+       
+            scrollView.addSubview(playerViewController.view)
+            self.addChild(playerViewController)
+            yPos = yPos + Int(UIScreen.main.bounds.height)
+        }
+        VideoSource.sharedInstance.videos.first?.player.play()
+        
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat((VideoSource.sharedInstance.videos.count)))
+        
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        // to ignore safe area...
+        scrollView.contentInsetAdjustmentBehavior = .never
+    }
+    
+    func makePlayerViewController(player: AVPlayer, yPos: CGFloat) -> AVPlayerViewController{
+        
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        playerViewController.showsPlaybackControls = false
+        playerViewController.videoGravity = .resizeAspectFill
+        playerViewController.view.backgroundColor = .white
+        playerViewController.view.frame = CGRect(x: 0, y: yPos, width:UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        return playerViewController
     }
 
 
