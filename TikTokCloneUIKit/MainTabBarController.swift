5698bac97a5d07590beb5d754ccd4a1fbef1e966
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-04-16 16:42:05 +0800

    Trying to use main tab bar controller

---
+++ TikTokCloneUIKit/MainTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabBarController.swift
+//  TikTokCloneUIKit
+//
+//  Created by Carlo Luis Martinez Bation on 16/4/21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+}
