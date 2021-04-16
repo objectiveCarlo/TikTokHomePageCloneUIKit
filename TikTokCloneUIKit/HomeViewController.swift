dc21f52ce5bd14236949207dea53a9ecb143244d
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-04-16 12:22:33 +0800

    Lets try scrolling using scrollviews

--- TikTokCloneUIKit/HomeViewController.swift
+++ TikTokCloneUIKit/HomeViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor.black
+        scrollView.contentSize = view.bounds.size
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(scrollView)
     }
 
 
