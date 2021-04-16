611550f83a5055448062e667918455b349b2b31d
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-04-16 11:50:45 +0800

    Initial Commit

---
+++ TikTokCloneUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokCloneUIKit
+//
+//  Created by Carlo Luis Martinez Bation on 16/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
