10518f163ae2866fb60a5f04737f5fad6b32a206
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-04-16 16:24:31 +0800

    Add looping

--- TikTokCloneUIKit/VideoBusinessRules/Video.swift
+++ TikTokCloneUIKit/VideoBusinessRules/Video.swift
@@ -7,27 +7,31 @@
 import AVFoundation
 struct Video : Identifiable {
     var id : Int
-    var player : AVPlayer
+    var player : AVQueuePlayer
 }
 
 class VideoSource {
     static let sharedInstance = VideoSource()
+    var looper: AVPlayerLooper?
     var currentIndex = 0 {
         willSet {
             videos[currentIndex].player.seek(to: .zero)
             videos[currentIndex].player.pause()
+            if let looper = looper {
+                looper.disableLooping()
+            }
         }
         didSet {
+            looper = AVPlayerLooper(player: videos[currentIndex].player, templateItem: videos[currentIndex].player.currentItem!)
             videos[currentIndex].player.play()
-            videos[currentIndex].player.actionAtItemEnd = .none
         }
     }
     var videos = [
-        Video(id: 0, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "1", ofType: "mp4")!))),
-        Video(id: 1, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "2", ofType: "mp4")!))),
-        Video(id: 2, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "3", ofType: "mp4")!))),
-        Video(id: 3, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "4", ofType: "mp4")!))),
-        Video(id: 4, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "5", ofType: "mp4")!))),
-        Video(id: 5, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "6", ofType: "mp4")!))),
+        Video(id: 0, player: AVQueuePlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "1", ofType: "mp4")!))),
+        Video(id: 1, player: AVQueuePlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "2", ofType: "mp4")!))),
+        Video(id: 2, player: AVQueuePlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "3", ofType: "mp4")!))),
+        Video(id: 3, player: AVQueuePlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "4", ofType: "mp4")!))),
+        Video(id: 4, player: AVQueuePlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "5", ofType: "mp4")!))),
+        Video(id: 5, player: AVQueuePlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "6", ofType: "mp4")!))),
     ]
 }
