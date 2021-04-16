7e33b005ac5a7ae5e171ce7704823afd11fc3bc6
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-04-16 17:11:41 +0800

    Transparent tab bar

--- TikTokCloneUIKit/AppDelegate.swift
+++ TikTokCloneUIKit/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let tabBar = UITabBar.appearance()
+        tabBar.barTintColor = UIColor.clear
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
         return true
     }
 
